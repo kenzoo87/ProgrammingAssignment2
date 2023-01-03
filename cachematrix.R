5d4e850929700d878675a65dd0edce8e1278cb28
Smahi <SMAHIYO1@novartis.net>
2023-01-03 11:12:48 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,50 @@
 
 ## Write a short comment describing this function
 
+library(MASS) # Library MASS is used to calculate inverse Matrix
+##use the template provided by the exercise and the "mean" example
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+      x <<- y
+      m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() {invers <-ginv(x) #used to obtain the inverse matrix
+                            invers%*%x
+                            }
+  list(set= set, get = get,
+        setinverse = setinverse,
+        getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
+##use the template provided by the exercise and the "mean" example
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+    m
+  }
+  data <-x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+
 }
+
+#test the function 
+mdat <- makeCacheMatrix(matrix(1:10,2,5))
+mdat$get()
+mdat$getinverse()
+cacheSolve(mdat)
+
+mdat$setinverse()
+
+#error
+cacheSolve(mdat)
+makeCacheMatrix(mdat)
+
